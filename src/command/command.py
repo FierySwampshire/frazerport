20e39620984798de67c96fe123353065d1b00069
FierySwampshire <woodsfiery@gmail.com>
2022-08-25 00:58:35 +0530

    added .gitignore, changes in command.py, logs

--- src/command/command.py
+++ src/command/command.py
@@ -1,4 +1,5 @@
 # This example requires the 'members' and 'message_content' privileged intents to function.
+import collections
 from typing import List
 from src.creds import get_secret_key
 import discord
@@ -22,16 +23,14 @@ bot = commands.Bot(command_prefix=')', description=description, intents=intents)
 async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     print('------')
-    print(dict(bot))
+    print(bot)
     print(dir(bot))
     print(bot.intents)
 
 
 @bot.command()
 async def role(ctx: Context):
-    print(ctx)
-    print(type(ctx))
-    print(dir(ctx))
+    log_guild(ctx)
     all_roles = ctx.guild._roles
     author_roles: List[discord.Role] = [all_roles.get(role, None) for role in ctx.author._roles if role in all_roles]
     await ctx.send(f'your roles are : {[i.name for i in author_roles]}')
@@ -125,3 +124,12 @@ async def _bot(ctx):
 async def _shirt(ctx):
     messages = ['yes, the shirt is cool', 'no, the shirt is not cool']
     await ctx.send(random.choice(messages))
+
+
+member = collections.namedtuple('member', 'id name discriminator bot nick')
+
+
+def log_guild(ctx: Context):
+    print(f'''Author = ({ctx.author.name}, {ctx.author.id}), Guild = ({ctx.guild.name} {ctx.guild.id}), Channel = ({ctx.channel.name}, {ctx.channel.id}),\
+    Message = ({ctx.message.content}), Members = {[member(id=i.id, name=i.name, discriminator=i.discriminator, bot=i.bot, nick=i.nick) for i in ctx.guild.members]}''')
+
